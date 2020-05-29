f2da022a656a2f9c7ad2761311f516d0e6ac6203
gektor <gektor89.89@yandex.ru>
2020-05-29 19:17:56 +0300

    +

--- mobile/src/main.js
+++ mobile/src/main.js
@@ -1,6 +1,9 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+import Vuex from 'vuex'
+import AppPodbor from './components/AppPodbor'
+import storePodbor from './store/podbor'
 import 'normalize.css'
 import './assets/css/fonts.css'
 import './bem.css'
@@ -12,6 +15,7 @@ import AdvantagesPopup from './assets/js/AdvantagesPopup/AdvantagesPopup'
 // Подключаем глобально jQuery
 let $ = window.$ = window.jQuery = require('jquery')
 
+Vue.use(Vuex)
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
@@ -20,4 +24,14 @@ $(document).ready(function () {
   new TabList()
   new ProductTab()
   new AdvantagesPopup()
+
+  /* Подбор товаров (на стр. оформление заказа) */
+  if ($('#vue-podbor').length) {
+    new Vue({
+      el: '#vue-podbor',
+      store: storePodbor,
+      components: { AppPodbor },
+      template: '<AppPodbor/>'
+    })
+  }
 })
